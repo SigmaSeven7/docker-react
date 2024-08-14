be1932cb1a3e3ba02dd572a2b0e00953f388b970
Oryan Tayer <oryantayer@gmail.com>
2024-08-14 11:27:14 +0300

    git add dockerfiles

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Hi there! I, Oryan, am  inside a React App. learn react. Click me to learn more about React.
         </a>
       </header>
     </div>
